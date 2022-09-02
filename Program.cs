7477d50ecbcb6af38908633bdccb6ac30adea7aa
vlada <vsh_o@ukr.net>
2022-09-02 11:35:56 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oop_lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
